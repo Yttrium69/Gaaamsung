ce98a4a1f862a648a8df6b242a4a031ec07898db
Yttrium69 <dmswldmsgp00@naver.com>
2023-11-20 20:28:48 +0900

    published right section completely, but not imlemented any functions

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -1,4 +1,4 @@
-import Label from "./Label";
+import '../CSS/base.scss';
 
 interface card_prop {
     label: number;
@@ -7,21 +7,71 @@ interface card_prop {
     img_path: string | null;
 }
 
+function Label(props:{label:number}):JSX.Element{
+    let label_text:string="";
+    switch (props.label){
+        case 0: {
+            label_text = "Cool scent";
+            break;
+        }
+        case 1: {
+            label_text = "Fresh scent";
+            break;
+        }
+        case 2: {
+            label_text = "Normal scent";
+            break;
+        }
+        case 3: {
+            label_text = "Cosy scent";
+            break;
+        }
+        case 4: {
+            label_text = "Flavorful scent";
+            break;
+        }
+    }
+    return (
+        <div className={"component_label"}>
+            <div className={`dot back_label_${props.label}`}></div>
+            <div className={`label_text font_label_${props.label}`}>{label_text}</div>
+        </div>
+)
+}
+
+function Tag(props:{tag_title:string}):JSX.Element{
+    return(
+        <div className={"component_tag"}>
+            {'#'+props.tag_title}
+        </div>
+    )
+}
+
+function Tags_container(props:{tags:string[]}):JSX.Element{
+    const tags_in_dom:JSX.Element[] = props.tags.map((title)=><Tag tag_title={title}></Tag>);
+    return(
+        <div className={"component_tags_container"}>
+            {tags_in_dom}
+        </div>
+    )
+}
+
 function Menu(props: card_prop): JSX.Element {
     let menu_img: JSX.Element = <div></div>;
     props.img_path ? menu_img =
-        <div className="img_container">
-            <img src={props.img_path!} alt="product image"></img>
-        </div> : <div></div>;
+        <div className="product_sample_img">
+            <div className="img_container">
+                <img src={props.img_path!} alt="product image"></img>
+            </div>
+        </div>
+         : <div className={"product_sample_img"}></div>;
     return (
         <div className="component_menu">
             {menu_img}
             <div className="contents_container">
                 <Label label={0}></Label>
                 <div className="title">{props.title}</div>
-                <div className="tags_container">
-                    <div className="tag">#유쾌한</div>
-                </div>
+                <Tags_container tags={props.tag}/>
             </div>
         </div>
     );
--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -3,6 +3,7 @@ import Right from './Right';
 import Left from './Left'
 import '../CSS/main.scss';
 
+
 function Main(): JSX.Element {
     return (
         <div className="page_main">
